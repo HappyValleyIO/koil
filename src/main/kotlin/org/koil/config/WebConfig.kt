d9d0ac054e3fad00812a981da64817a2bf5bee8a
Stephen van Beek <stephen@happyvalley.io>
2021-07-16 15:42:12 +0100

    Implemented remember me on login

---
+++ src/main/kotlin/org/koil/config/WebConfig.kt
@@ -0,0 +1,29 @@
+§package org.koil.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.CacheControl
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.concurrent.TimeUnit
+import javax.sql.DataSource
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/")
+            .setCacheControl(
+                CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic()
+            )
+    }
+
+    @Bean
+    fun rememberMeTokenRepository(dataSource: DataSource): PersistentTokenRepository {
+        return JdbcTokenRepositoryImpl().apply {
+            setDataSource(dataSource)
+            setCreateTableOnStartup(false)
+        }
+    }
+}
--- src/webapp/cypress/integration/login.js
+++ src/webapp/cypress/integration/login.js
@@ -12,23 +12,39 @@ describe(`User login flows on ${size}`, () => {
     })
 
     it(`should login successfully`, () => {
-      cy.get('@account').then(account => {
-        cy.get('[data-test=login-email-input]').type(account.email)
-        cy.get('[data-test=login-password-input]').type(account.passwd)
-        cy.get('[data-test=login-submit]').click()
-        cy.url().should('include', '/dashboard')
-      })
+        cy.get('@account').then(account => {
+            cy.get('[data-test=login-email-input]').type(account.email)
+            cy.get('[data-test=login-password-input]').type(account.passwd)
+            cy.get('[data-test=login-submit]').click()
+            cy.url().should('include', '/dashboard')
+        })
     });
 
+    it('should remember when remember-me is checked on login', function () {
+        cy.get('@account').then(account => {
+            cy.get('[data-test=remember-me]').check()
+            cy.get('[data-test=login-email-input]').type(account.email)
+            cy.get('[data-test=login-password-input]').type(account.passwd)
+            cy.get('[data-test=login-submit]').click()
+            cy.get('[data-test=login-submit]').click()
+
+            cy.url().should('include', 'dashboard')
+
+            cy.clearCookie('SESSION')
+            cy.visit('/dashboard')
+            cy.url().should('include', 'dashboard')
+        })
+    })
+
     it(`should show failure error message`, () => {
-      cy.get('@account').then(account => {
-        cy.get('[data-test=bad-credentials-error]').should('not.exist')
-        cy.get('[data-test=login-email-input]').type(account.email)
-        cy.get('[data-test=login-password-input]').type(account.passwd + 'X')
-        cy.get('[data-test=login-submit]').click()
-        cy.url().should('include', '/auth/login')
-        cy.get('[data-test=bad-credentials-error]').should('be.visible')
-      })
+        cy.get('@account').then(account => {
+            cy.get('[data-test=bad-credentials-error]').should('not.exist')
+            cy.get('[data-test=login-email-input]').type(account.email)
+            cy.get('[data-test=login-password-input]').type(account.passwd + 'X')
+            cy.get('[data-test=login-submit]').click()
+            cy.url().should('include', '/auth/login')
+            cy.get('[data-test=bad-credentials-error]').should('be.visible')
+        })
     })
   })
 });
