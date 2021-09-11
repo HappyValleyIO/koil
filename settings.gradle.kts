0fb2c99271ac15a4690ac9dc2bba1d587a2b3238
Stephen van Beek <35293048+stepbeekio@users.noreply.github.com>
2021-09-11 13:21:26 +0100

    Fixed cypress tests and updated dependencies (#48)
    * Fixed cypress tests and updated dependencies
    
    * Fixed heroku deploy
    
    * Using better machine type on semaphore
    
    * Revert "Using better machine type on semaphore"
    
    This reverts commit dadeb21104796f7f9bbe36b7f9c7ced663b3e91b.
    
    * Fixing heroku build
    
    * Disabling plain jar generation to fix heroku

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "koil"
+
+plugins {
+    id("com.gradle.enterprise") version ("3.6.4")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
---
+++ src/webapp/cypress/integration/admin/impersonate.js
@@ -0,0 +1,48 @@
+const sizes = ['iphone-6', 'iphone-x', 'ipad-mini', 'macbook-13'];
+
+
+sizes.forEach(size => {
+    function isDesktopStyle() {
+        return ['ipad-mini', 'macbook-13'].includes(size)
+    }
+
+    describe(`Admin impersonating users on ${size}`, () => {
+
+        beforeEach(() => {
+            cy.viewport(size)
+            cy.createRandomAccount()
+            cy.clearCookies()
+            cy.visit("/auth/login")
+        })
+
+        it('should allow an admin to impersonate another user', () => {
+            cy.loginAsAdmin()
+            cy.get('@account').then(account => {
+                cy.visit("/admin?size=10000")
+                cy.get(`[data-test="account-row-${account.email}"]`)
+                    .within(() => {
+                        cy.get('[data-test=user-details]').click()
+                    })
+                cy.get('[data-test=impersonate]').click()
+                cy.get('[data-test=dashboard-index]').should('exist')
+
+                if (isDesktopStyle()) {
+                    cy.get('[data-test=user-handle]').contains(`@${account.username}`)
+                    cy.get('[data-test=end-impersonation]').should('be.visible').click()
+                    cy.get('[data-test=end-impersonation]').should('not.exist')
+                    cy.get('[data-test=user-handle]').contains(`@DefaultAdmin`)
+                } else {
+                    cy.get('[data-test=menu-button]').click()
+                    cy.get('[data-test=user-handle-mobile]').contains(`@${account.username}`)
+                    cy.get('[data-test=mobile-navbar]').within(() => {
+                        cy.get('[data-test=end-impersonation-mobile]').should('be.visible').click()
+                        cy.get('[data-test=end-impersonation-mobile]').should('not.exist')
+                    })
+
+                    cy.get('[data-test=menu-button]').click()
+                    cy.get('[data-test=user-handle-mobile]').contains('@DefaultAdmin')
+                }
+            })
+        })
+    })
+});
