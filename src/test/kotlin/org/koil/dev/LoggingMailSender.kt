a8c1827e082ef3caaa6d4efe5c1d339e0c49c946
hilary <hilary@happyvalley.io>
2021-11-04 15:07:50 +0000

    Added the "Last-sent" dev controller endpoint.
    - This is not currently used for any tests, it's just for manual use in dev so far. It will run into concurrency issues if it's used by cypress tests that share an instance of the LoggingMailSender instance since it's an in-memory list of all sent emails.
    - I decided to enrich the LoggingMailSender class instead of updating the Notifications Service because I thought it would help keep dev only code in a dev-only class. I've tested this and it seems to serve up the email properly so I think it's working pretty successfully as a solution.
    - At the moment- it has a "to" filter but nothing else, it might be good to get an additional requestParam to get the "type" of the email so that you can get your "welcome" email or your "verification" email, etc.

--- src/test/kotlin/org/koil/dev/LoggingMailSender.kt
+++ src/test/kotlin/org/koil/dev/LoggingMailSender.kt
@@ -7,13 +7,21 @@ import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessagePreparator
 import java.io.InputStream
 import java.util.*
+import javax.mail.Message
 import javax.mail.Session
 import javax.mail.internet.MimeMessage
+
+data class SentEmail(val to: String, val subject: String, val body: String)
 
 class LoggingMailSender : JavaMailSender {
     companion object {
         val LOG: Logger = LoggerFactory.getLogger(LoggingMailSender::class.java)!!
     }
+
+    private val sentEmails: MutableList<SentEmail> = mutableListOf()
+
+    fun getEmails(): List<SentEmail> = sentEmails
+
 
     override fun createMimeMessage(): MimeMessage {
         return MimeMessage(Session.getDefaultInstance(Properties()))
@@ -24,11 +32,15 @@ class LoggingMailSender : JavaMailSender {
     }
 
     override fun send(mimeMessage: MimeMessage) {
+        sentEmails.add(SentEmail(mimeMessage.getRecipients(Message.RecipientType.TO).joinToString(","), mimeMessage.subject.toString(), mimeMessage.content.toString() ))
         LOG.info("Sending email $mimeMessage")
     }
 
     override fun send(vararg mimeMessages: MimeMessage?) {
         mimeMessages.forEach {
+            it?.also{ message ->
+                sentEmails.add(SentEmail(message.getRecipients(Message.RecipientType.TO).joinToString(","), message.subject.toString(), message.content.toString() ))
+            }
             LOG.info("Sending email $it")
         }
     }
@@ -44,11 +56,15 @@ class LoggingMailSender : JavaMailSender {
     }
 
     override fun send(simpleMessage: SimpleMailMessage) {
+        sentEmails.add(SentEmail(simpleMessage.to.orEmpty().joinToString(","), simpleMessage.subject.toString(), simpleMessage.text.orEmpty() ))
         LOG.info("Sending email $simpleMessage")
     }
 
     override fun send(vararg simpleMessages: SimpleMailMessage?) {
         simpleMessages.forEach {
+            it?.also{ message ->
+                sentEmails.add(SentEmail(message.to.contentToString(), message.subject.toString(), message.text.orEmpty() ))
+            }
             LOG.info("Sending email $it")
         }
     }
